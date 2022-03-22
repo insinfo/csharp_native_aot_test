a82678f3d642d5125314a6b87af58a9a634b3f74
Isaque Neves <insinfo2008@gmail.com>
2022-03-22 11:42:43 -0300

    add simple example program

--- program.php
+++ program.php
@@ -1,3 +1,22 @@
 ﻿<?php
 
-echo "Hello World!";
+require_once 'vendor/autoload.php';
+
+// Parse PDF file and build necessary objects.
+$parser = new \Smalot\PdfParser\Parser();
+$pdf = $parser->parseFile('C:\MyCsharpProjects\fsbackup2\1417.pdf');
+$text = $pdf->getPages()[0]->getText();
+//print text
+echo $text;
+
+use phpseclib3\Net\SFTP;
+//connect to SSH/SFTP server and list directory
+$sftp = new SFTP('192.168.133.13');
+$sftp->login('isaque.neves', 'Ins257257');
+$sftp->chdir('/var/www');
+//Directory Listing
+foreach ($sftp->nlist() as &$directoryName) {
+    echo $directoryName . "\r\n";
+}
+
+echo "End Hello World!";
